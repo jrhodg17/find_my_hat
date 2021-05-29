844fa54cdf1019a851e7aa894561dda30fcea4c3
johnhodge <jrhodg17@g.holycross.edu>
2021-05-29 11:49:37 -0400

    Complete generateField function

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-const importedFunc = require('./playGame.js');
+const importedPlayGame = require('./playGame.js');
 
 const hat = '^';
 const hole = 'O';
@@ -23,22 +23,28 @@ class Field {
         let fieldArray = [];
 
         for (let i = 0; i < heightRange.length; i++) {
-            fieldArray.push(widthRange);
+            fieldArray.push([]);
         }
+
+        for (let i = 0; i < fieldArray.length; i++) {
+            for (let j = 0; j < widthRange.length; j++) {
+                const chars = [fieldCharacter, fieldCharacter, hole];
+                let randomChar = chars[Math.floor(Math.random() * chars.length)];
+                fieldArray[i].push(randomChar);
+            }
+        }
+
+        const randomRow = heightRange[Math.floor(Math.random() * heightRange.length)];
+        const randomColumn = widthRange[Math.floor(Math.random() * widthRange.length)];
+
+        fieldArray[randomRow][randomColumn] = hat;
+        fieldArray[0][0] = pathCharacter;
         
         return fieldArray;
     }
 }
 
-// const myField = new Field([
-//     [pathCharacter, fieldCharacter, hole],
-//     [fieldCharacter, hole, fieldCharacter],
-//     [fieldCharacter, hat, fieldCharacter]
-// ]);
-
-// importedFunc.playGame(myField);
-
-const genField = Field.generateField(3, 4);
+const genField = Field.generateField(4, 6);
 const myField = new Field(genField);
 
-myField.print();
+importedPlayGame.playGame(myField);
